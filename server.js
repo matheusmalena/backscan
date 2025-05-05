639617888093a755bcb555e64e85f8fe4e8b6cb5
Matheus Malena <matheus@tew.one>
2025-05-05 11:00:05 -0300

    update

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "7741903708:AAGZQpnoLQIC-9WowgtBJUpqTTP8BAsaLO8";
+const TELEGRAM_CHAT_ID = "-4728722007";
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
